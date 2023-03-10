fb1a14d7cf001377df8c718b7f815bb2dfa20376
Mrc <qd17777064604@qq.com>
2023-03-10 18:54:26 +0800

    23/3/10

--- src/test/java/com/gx/ServerApplicationTests.java
+++ src/test/java/com/gx/ServerApplicationTests.java
@@ -1,24 +1,31 @@
 package com.gx;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.gx.entity.Users;
 import com.gx.mapper.UsersMapper;
+import com.gx.service.IUsersService;
 import org.apache.catalina.User;
+import org.apache.catalina.Wrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.xml.transform.Result;
 
 @SpringBootTest
 class ServerApplicationTests {
 
     @Autowired
-    protected UsersMapper usersMapper;
+    protected IUsersService usersService;
 
     @Test
     void contextLoads() {
-        Users user = new Users();
-        user.setAccount("123");
-        user.setPassword("321");
-        //usersMapper.selectOne(user);
+        R login = usersService.login("admin", "123");
+        System.out.println(login);
+
+        R selectUser = usersService.selectUser(2);
+        System.out.println(selectUser);
     }
 
 }
